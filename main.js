c602c71dec030e421bf25863d4eb07683a804bc0
Rahul Sharma <rahu8299@gmail.com>
2023-02-06 17:20:03 +0530

    .

--- main.js
+++ main.js
@@ -1,2 +1,7 @@
-console.log('hello');
-let some
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+app.use("/", express.static("public"));
+app.listen(8080);
+print("server running at: http://localhost:8080/");
